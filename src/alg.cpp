501fbfa6122c0a9fb43aa4174a5b1899cdee706d
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-02-19 13:09:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,8 @@
 
 
 bool checkPrime(uint64_t value) {
-    int count_d = 0;
-    for (int i = 1; i <= value; i++) {
+    int64_t count_d = 0;
+    for (int64_t i = 1; i <= value; i++) {
         if (value % i == 0) {
             ++count_d;
         }
@@ -18,11 +18,11 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    const int size = 100;
-    int arr[size] = { 0 };
-    int p = 0;
-    int i = 0, j = 0, t = 0;
-    for (i = 0; i < 100; i++) {
+    const int64_t size = 10000;
+    int64_t arr[size] = { 0 };
+    int64_t p = 0;
+    int64_t i = 0, j = 0, t = 0;
+    for (i = 0; i < 10000; i++) {
         t = 0;
         for (j = 1; j <= i; j++) {
             if ((i % j) == 0) {
@@ -43,11 +43,11 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-    const int size = 100;
-    int arr[size] = { 0 };
-    int p = 0;
-    int i = 0, j = 0, t = 0;
-    for (i = 0; i < 100; i++) {
+    const int64_t size = 10000;
+    int64_t arr[size] = { 0 };
+    int64_t p = 0;
+    int64_t i = 0, j = 0, t = 0;
+    for (i = 0; i < 10000; i++) {
         t = 0;
         for (j = 1; j <= i; j++) {
             if ((i % j) == 0) {
@@ -59,7 +59,7 @@ uint64_t nextPrime(uint64_t value) {
             p++;
         }
     }
-    for (int i = 0; i < p; i++) {
+    for (int64_t i = 0; i < p; i++) {
         if (arr[i] == value) {
             return arr[i + 1];
         }
@@ -68,11 +68,11 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    const int size = 100;
-    int arr[size] = { 0 };
-    int pos = 0;
-    int i = 0, j = 0, t = 0;
-    for (i = 0; i < 100; i++) {
+    const int64_t size = 10000;
+    int64_t arr[size] = { 0 };
+    int64_t pos = 0;
+    int64_t i = 0, j = 0, t = 0;
+    for (i = 0; i < 10000; i++) {
         t = 0;
         for (j = 1; j <= i; j++) {
             if ((i % j) == 0) {
@@ -84,8 +84,8 @@ uint64_t sumPrime(uint64_t hbound) {
             pos++;
         }
     }
-    int sum = 0;
-    for (int i = 0; i < hbound - 1; i++) {
+    int64_t sum = 0;
+    for (int64_t i = 0; i < hbound - 1; i++) {
         sum += arr[i];
     }
     return sum;
