c37605e02278c9eb88e6135f53b039b2b6108e3a
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-02-25 15:02:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -67,8 +67,7 @@ uint64_t nextPrime(uint64_t value) {
     return 0;
 }
 
-uint64_t sumPrime(uint64_t  hbound)
-{
+uint64_t sumPrime(uint64_t  hbound) {
     std::int64_t sum = 0;
     for (int i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
